1fe0be866bd48b53ff62079fdc2a5bed4bc5428b
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-30 12:47:12 -0700

    initial setup of chat db schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,8 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
+  'id' 'datatype',
+  'userid' 'integer',
+  'message' 'varchar',
+  'roomname' 'integer'
+
   /* Describe your table here.*/
 );
+
+CREATE TABLE users (
+  'id' 'integer',
+  'username' 'varchar'
+);
+
+CREATE TABLE rooms (
+  'id' 'integer',
+  'roomane' 'varchar'
+)
 
 /* Create other tables and define schemas for them here! */
 
