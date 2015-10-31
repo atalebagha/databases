acfeddf37a67155852f88936b4a571fc1d381df8
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-31 14:59:30 -0700

    updated posting of messages - still failing

--- server/models/index.js
+++ server/models/index.js
@@ -6,28 +6,43 @@ var insertHelper = function ( table, obj, callback ) {
   _.each(obj, function(val) {
     val = db.escape(val);
   });
-  var sql = "INSERT INTO " + table + " SET ??";
+  var sql = 'INSERT INTO ' + table + ' SET ?';
 
   db.query(sql, obj, callback);
 };
 
 var selectHelper = function ( table, columns, field, args, callback ) {
-  var sql = "SELECT ?? FROM ??" // WHERE ?? = ? ORDER BY ??;
-  columns = columns || "*";
-  field = field || "id";
-  sql = args ? sql + " WHERE ?? = ?" : sql;
-  var inserts = ( args ? [columns, table, field, args] : [columns, table] );
-  sql = mysql.format(sql, inserts);
-  console.log(sql);
+  var sql = 'SELECT ?? FROM ??';
+  var insert = [columns, table];
+  columns = columns || '*';  // 'ABDUL'  '/'ABDUL/''
+  field = field || 'id';
+  if (args) {
+    sql = sql + ' WHERE ?? = ?';
+    insert.push(field, args);
+  }
+  sql = mysql.format(sql, insert);
+  console.log(sql); //TODO: REMOVE
 
   var select = {
     sql: sql,
     timeout: 20000
   };
   db.query(select, callback);
+  //console.log(result);
+  //return result;
 };
 
+var queryReturn = function (err, result, el) {
+  if (err) throw err;
+  if (el) return result[el];
+  return result;
+};
 
+var queryAssign = function (err, result, el, val) {
+  if (err) throw err;
+  if (el) val = result[el];
+  val = result;
+};
 
 module.exports = {
   messages: {
@@ -35,33 +50,43 @@ module.exports = {
       selectHelper('messages', columns, field, args, callback);
     },
     post: function (message, roomname, username, callback ) { // maybe we can pull user_id and
+      var roomid;
+      roomid = selectHelper('rooms', 'id', 'roomname', roomname, function (err, result) {
+        if (err) throw err;//{
+          // console.log("creating Roomname");
+          // console.log(exports.rooms);
+          // that.rooms.post( roomname, queryCallback(err, result, 'insertId'));
+        // }
+         //  // add error handler to add room in case it doesn't exist
+        console.log(result);
+       roomid = result.id;
+
+              console.log("roomid:  " + roomid);
+      // do you we get error or empty result?
+
       var obj = {
-        'room_id': selectHelper('rooms', 'id', 'roomname', db.escape(roomname), function (err, result) {
-          if (err) throw err;// if first error, it will attempt to add user name below
-          if (!result) {
-            console.log("creating Roomname");
-            exports.rooms.post( roomname, function (err, result) {
-              if (err) throw err;
-              return result.insertId; // must add insertId to return ID only
-            });
-          }
-           //  // add error handler to add room in case it doesn't exist
-          return result;
-        }),
-
+        'room_id': 1,
         text: message,
+        'user_id': 1  // TEMPRORARY
 
-        'user_id': selectHelper('users', 'id', 'username', db.escape(username), function (err, result) {
-          if (err) { // if first error, it will attempt to add user name below
-            console.log("creating username");
-            exports.users.post( username, function (err, result) {
-              if (err) throw err;
-              return result.insertId; // must add insertId to return id only
-            });
-          } //
-          return result;
-        }) };
+        // 'user_id': selectHelper('users', 'id', 'username', db.escape(username), function (err, result) {
+        //   if (err) { // if first error, it will attempt to add user name below
+        //     console.log("creating username");
+        //     exports.users.post( username, function (err, result) {
+        //       if (err) throw err;
+        //       return result.insertId; // must add insertId to return id only
+        //     });
+        //   } //
+        //   return result;
+        // })
+      };
       insertHelper( 'messages', obj , callback );
+      });
+      // if err
+      //    create room (or user)
+      //    run insert again!
+      //
+
     } // a function which can be used to insert a message into the database
   },
 
@@ -88,3 +113,4 @@ module.exports = {
     }
   }
 };
+var that = module.exports;
