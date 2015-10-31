e4020b39ec4def47e3074077355faeeeaac94738
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-30 20:57:04 -0700

    added controller route object property for different routes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,19 +5,21 @@ var bluebird = require('bluebird');
 
 
 module.exports = {
-  messages: {
-    get: function (req, res) {
-      models.messages.get(['message', 'created', 'user_id'], '*', '', function (err, result) {
-        res.json(result);
-      })
-    }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
-  },
+  '/classes/messages': {
+    messages: {
+      get: function (req, res) {
+        models.messages.get(['message', 'created', 'user_id'], '*', '', function (err, result) {
+          res.json(result);
+        })
+      }, // a function which handles a get request for all messages
+      post: function (req, res) {} // a function which handles posting a message to the database
+    },
 
-  users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    users: {
+      // Ditto as above
+      get: function (req, res) {},
+      post: function (req, res) {}
+    }
   }
 };
 
