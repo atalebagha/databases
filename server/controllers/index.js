4b6f6ac50bf1caf6d58177cbe1bf26d3475a8784
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-30 20:09:28 -0700

    fixed db connection, connection works

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,16 @@
 var models = require('../models');
 var bluebird = require('bluebird');
+// var models = Promise.promisify(require("../models"))
 
 
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get(['message', 'created', 'user_id'], '*', '', function (err, result) {
+
+      })
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
