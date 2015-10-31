526f082b87f8d4eae6da71890608f3bae5a51d73
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-31 14:59:04 -0700

    updated message post

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,11 +9,10 @@ module.exports = {
     get: function (req, res) {
       models.messages.get('text', '', '', function (err, result) {
         if (err) throw err;
-        console.log(result);
+        //console.log(result);
         res.json(result);
       });
     }, // a function which handles a get request for all messages
-
 
     post: function (req, res) {
       // phony data placed below.  data will be received as property of req
