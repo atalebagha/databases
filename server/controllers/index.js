3f71e3c12de246cf6cd649edbccca30956d5dc8a
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-31 17:35:00 -0700

    completely changed our SQL query statements, posts are still not working

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,20 +7,39 @@ var bluebird = require('bluebird');
 module.exports = {
   messages : {
     get: function (req, res) {
-      models.messages.get('text', '', '', function (err, result) {
-        if (err) throw err;
-        //console.log(result);
-        res.json(result);
+      models.messages.get(function (results)  {
+        // console.log("Hi I am models get controller and I am running");
+        // console.log(results);
+        res.json(results);
       });
+
+      // models.messages.get('text', '', '', function (err, result) {
+      //   if (err) throw err;
+      //   //console.log(result);
+      //   res.json(result);
+      // });
     }, // a function which handles a get request for all messages
 
     post: function (req, res) {
-      // phony data placed below.  data will be received as property of req
-      models.messages.post('Hi there!', 'Lobby', 'Abdul', function (err, result) {
+      console.log(req.body);
+      // var roomname = req.body['roomname'];
+      // var text = req.body['text'];
+      // var username = req.body['username'];
+      var params = [req.body['text'], req.body['username'], req.body['roomname']];
+
+      models.messages.post(params, function (err, results) {
         if (err) throw err;
-        res.status(201);
-        res.end("Success");
+        console.log(results);
+        res.sendStatus(201);
       });
+
+
+      // phony data placed below.  data will be received as property of req
+      // models.messages.post('Hi there!', 'Lobby', 'Abdul', function (err, result) {
+      //   if (err) throw err;
+      //   res.status(201);
+      //   res.end("Success");
+      // });
     } // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -46,48 +46,67 @@ var queryAssign = function (err, result, el, val) {
 
 module.exports = {
   messages: {
-    get: function (columns, field, args, callback) {
-      selectHelper('messages', columns, field, args, callback);
-    },
-    post: function (message, roomname, username, callback ) { // maybe we can pull user_id and
-      var roomid;
-      roomid = selectHelper('rooms', 'id', 'roomname', roomname, function (err, result) {
-        if (err) throw err;//{
-          // console.log("creating Roomname");
-          // console.log(exports.rooms);
-          // that.rooms.post( roomname, queryCallback(err, result, 'insertId'));
-        // }
-         //  // add error handler to add room in case it doesn't exist
-        console.log(result);
-       roomid = result.id;
+    get: function (callback) {
+      var queryStr = ["SELECT messages.id, messages.text, rooms.roomname, users.username",
+                      "FROM messages LEFT OUTER JOIN users ON messages.user_id = users.id",
+                      "LEFT OUTER JOIN rooms ON messages.room_id = rooms.id",
+                      "ORDER BY messages.id DESC;"].join(' ');
 
-              console.log("roomid:  " + roomid);
-      // do you we get error or empty result?
-
-      var obj = {
-        'room_id': 1,
-        text: message,
-        'user_id': 1  // TEMPRORARY
-
-        // 'user_id': selectHelper('users', 'id', 'username', db.escape(username), function (err, result) {
-        //   if (err) { // if first error, it will attempt to add user name below
-        //     console.log("creating username");
-        //     exports.users.post( username, function (err, result) {
-        //       if (err) throw err;
-        //       return result.insertId; // must add insertId to return id only
-        //     });
-        //   } //
-        //   return result;
-        // })
-      };
-      insertHelper( 'messages', obj , callback );
+      db.query(queryStr, function(err, results){
+        callback(results);
       });
-      // if err
-      //    create room (or user)
-      //    run insert again!
-      //
+      // selectHelper('messages', columns, field, args, callback);
+    },
+    post: function (params, callback) { // maybe we can pull user_id and
+      var queryStr = ["INSERT INTO messages(text, user_id, room_id)",
+                      "VALUES (?, (SELECT id FROM users WHERE username = ? LIMIT 1),",
+                      "(SELECT id FROM rooms WHERE roomname = ? LIMIT 1));"].join(' ');
+      //queryStr = mysql.format(queryStr, params);
+      console.log("@@@@@@@@@@@@", params);
+      console.log("@@@@@@@@@@@@", queryStr);
+      db.query(queryStr, params, function(err, results){
+        callback(results);
+      });
+    }
+    // post: function (message, roomname, username, callback ) { // maybe we can pull user_id and
+    //   var roomid;
+    //   roomid = selectHelper('rooms', 'id', 'roomname', roomname, function (err, result) {
+    //     if (err) throw err;//{
+    //       // console.log("creating Roomname");
+    //       // console.log(exports.rooms);
+    //       // that.rooms.post( roomname, queryCallback(err, result, 'insertId'));
+    //     // }
+    //      //  // add error handler to add room in case it doesn't exist
+    //     console.log(result);
+    //    roomid = result.id;
 
-    } // a function which can be used to insert a message into the database
+    //           console.log("roomid:  " + roomid);
+    //   // do you we get error or empty result?
+
+    //   var obj = {
+    //     'room_id': 1,
+    //     text: message,
+    //     'user_id': 1  // TEMPRORARY
+
+    //     // 'user_id': selectHelper('users', 'id', 'username', db.escape(username), function (err, result) {
+    //     //   if (err) { // if first error, it will attempt to add user name below
+    //     //     console.log("creating username");
+    //     //     exports.users.post( username, function (err, result) {
+    //     //       if (err) throw err;
+    //     //       return result.insertId; // must add insertId to return id only
+    //     //     });
+    //     //   } //
+    //     //   return result;
+    //     // })
+    //   };
+    //   insertHelper( 'messages', obj , callback );
+    //   });
+    //   // if err
+    //   //    create room (or user)
+    //   //    run insert again!
+    //   //
+
+    // } // a function which can be used to insert a message into the database
   },
 
   users: {
